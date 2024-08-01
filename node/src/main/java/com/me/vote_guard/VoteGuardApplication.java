ff7ffb0ca6482c92988846160e07d34653c34279
Georgi Grigorov <georgigrigorov@Georgis-MacBook-Pro-2.local>
2024-08-01 15:04:35 +0300

    initialized node module

---
+++ node/src/main/java/com/me/vote_guard/VoteGuardApplication.java
@@ -0,0 +1,13 @@
+package com.me.vote_guard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteGuardApplication.class, args);
+	}
+
+}
